d949f9df9e6ea3bcee037b03b619c69f215fa942
lucas.lvn <lucas.lvn97439@gmail.com>
2024-08-09 15:48:46 +0400

    commit

--- assets/Scripts/hud.gd
+++ assets/Scripts/hud.gd
@@ -10,15 +10,14 @@ func _ready() -> void:
 var auguille_degree: int = 38
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$Score.text = str(Global.score)
-	
+	$BoxContainer/Control/Score.text = str(Global.score)
 	if self.get_parent().fuel == 4:
-		$CadranFuel/Aiguille.rotation_degrees  = 38
+		$BoxContainer/CadranFuel/Aiguille.rotation_degrees = 38
 	if self.get_parent().fuel == 3:
-		$CadranFuel/Aiguille.rotation_degrees  = 19
+		$BoxContainer/CadranFuel/Aiguille.rotation_degrees  = 19
 	if self.get_parent().fuel == 2:
-		$CadranFuel/Aiguille.rotation_degrees  = 0
+		$BoxContainer/CadranFuel/Aiguille.rotation_degrees  = 0
 	if self.get_parent().fuel == 1:
-		$CadranFuel/Aiguille.rotation_degrees  = -19
+		$BoxContainer/CadranFuel/Aiguille.rotation_degrees  = -19
 	if self.get_parent().fuel == 0:
-		$CadranFuel/Aiguille.rotation_degrees  = -38
+		$BoxContainer/CadranFuel/Aiguille.rotation_degrees  = -38
